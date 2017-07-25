9ad44c1a1dd49009d26465be322c6442cc43c13f
amin <amin@localhost>
2017-07-25 16:40:45 +0430

    F1: Google search result Added Core Service got updated

--- src/runner/run.java
+++ src/runner/run.java
@@ -6,6 +6,7 @@
 package runner;
 
 import core.CoreServices;
+import features.F1;
 import features.F23;
 import features.F24;
 
@@ -25,6 +26,12 @@ public class run {
 //        System.out.println(F24.getNumberOfPageWatchers(3649517));
 //        System.out.println(core.parseInt("123,234"));
 //        CoreServices.getNumberOfSearchResult("link");
+
+        String[] qu = {"ios", "what", "android", "apple"};
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(F1.getNumberOfGoogleSearchResult(qu[i]));
+        }
     }
-    
+
 }
